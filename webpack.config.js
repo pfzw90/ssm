154b03135673e25e883d9b03b27e8f5069e6152e
Natalya Chekhonina <pfzw90@gmail.com>
2024-01-02 15:36:15 +0300

    .

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+};
